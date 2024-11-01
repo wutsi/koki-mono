b744b8fd8a9f991c9532c851484105d0652b4ffd
wutsi <herve.tchepannou@wutsi.com>
2024-11-01 06:27:51 -0400

    update dependencies

--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/common/dto/HttpHeader.kt
+++ modules/koki-dto/src/main/kotlin/com/wutsi/koki/common/dto/HttpHeader.kt
@@ -2,4 +2,9 @@ package com.wutsi.koki.common.dto
 
 object HttpHeader {
     val TENANT_ID = "X-Tenant-ID"
+    val TRACE_ID = "X-Trace-ID"
+    val REQUEST_ID = "X-Request-ID"
+    val CLIENT_ID = "X-Client-ID"
+    val DEVICE_ID = "X-Device-ID"
+    val CLIENT_INFO = "X-Client-Info"
 }
