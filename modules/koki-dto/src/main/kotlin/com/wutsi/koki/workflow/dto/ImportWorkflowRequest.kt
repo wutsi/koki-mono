b6608a2c175fb2b84cc545d92e8caaa4b7caf559
wutsi <herve.tchepannou@wutsi.com>
2024-11-02 08:35:40 -0400

    add role

--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/workflow/dto/ImportWorkflowRequest.kt
+++ modules/koki-dto/src/main/kotlin/com/wutsi/koki/workflow/dto/ImportWorkflowRequest.kt
@@ -5,17 +5,19 @@ data class ImportWorkflowRequest(
 )
 
 data class ActivityData(
+    @Deprecated("")
     val code: String = "",
     val type: ActivityType = ActivityType.UNKNOWN,
     val name: String = "",
     val description: String? = null,
     val requiresApproval: Boolean = true,
     val tags: Map<String, String> = emptyMap(),
-    val predecessors: List<String> = emptyList()
+    val predecessors: List<String> = emptyList(),
+    val role: String? = null,
 )
 
 data class WorkflowData(
     val name: String = "",
-    val description: String = "",
+    val description: String? = null,
     val activities: List<ActivityData> = emptyList(),
 )
--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/workflow/dto/Workflow.kt
+++ modules/koki-dto/src/main/kotlin/com/wutsi/koki/workflow/dto/Workflow.kt
@@ -1,13 +1,15 @@
 package com.wutsi.koki.workflow.dto
 
+import com.wutsi.koki.tenant.dto.Role
 import java.util.Date
 
 data class Workflow(
     val id: Long = -1,
     val name: String = "",
-    val description: String = "",
-    val active: Boolean = true,
+    val description: String? = null,
+    val active: Boolean = false,
     val createdAt: Date = Date(),
     val modifiedAt: Date = Date(),
     val activities: List<Activity> = emptyList(),
+    val roles: List<Role> = emptyList()
 )
