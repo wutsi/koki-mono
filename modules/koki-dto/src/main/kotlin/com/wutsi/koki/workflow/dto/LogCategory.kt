9d6dd78b3fd142e7ab093dad6e71fc76fd4b58d8
wutsi <herve.tchepannou@wutsi.com>
2024-11-03 06:05:05 -0500

    remove unused classes

--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/workflow/dto/LogCategory.kt
+++
@@ -1,8 +0,0 @@
-package com.wutsi.koki.workflow.dto
-
-enum class LogCategory {
-    UNKNOWN,
-    INFO,
-    WARNING,
-    ERROR
-}
--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/workflow/dto/TaskEventType.kt
+++
@@ -1,7 +0,0 @@
-package com.wutsi.koki.workflow.dto
-
-enum class TaskEventType {
-    UNKNOWN,
-    STATUS_CHANGED,
-    APPROVAL_CHANGED,
-}
