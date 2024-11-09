d62873adde7f6bcc7311b36a3ff55936bd9c5fd8
wutsi <herve.tchepannou@wutsi.com>
2024-11-09 10:32:04 -0500

    remove unused fields

--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/form/dto/ImportFormRequest.kt
+++ modules/koki-dto/src/main/kotlin/com/wutsi/koki/form/dto/ImportFormRequest.kt
@@ -1,9 +1,5 @@
 package com.wutsi.koki.form.dto
 
-import jakarta.validation.constraints.NotEmpty
-
 data class ImportFormRequest(
-    @NotEmpty() val title: String = "",
-    val description: String? = null,
     val content: FormContent = FormContent()
 )
