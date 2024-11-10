7eed12c49b4bbb076a9d1091a3bb04ba1ebff5e6
wutsi <herve.tchepannou@wutsi.com>
2024-11-10 11:55:51 -0500

    fix script

--- modules/koki-server/src/test/resources/db/test/form/ExportFormHTMLDataEndpoint.sql
+++ modules/koki-server/src/test/resources/db/test/form/ExportFormHTMLDataEndpoint.sql
@@ -18,9 +18,9 @@ INSERT INTO T_ACTIVITY(id, workflow_fk, name, type, requires_approval)
            (103, 100, 'SUBMIT',  4, false),
            (104, 100, 'STOP',    2, false);
 
-INSERT INTO T_WORKFLOW_INSTANCE(id, tenant_fk, workflow_fk, approver_fk, status, start_at, state, parameters)
-    VALUES ('wi-100-01', 1, 100, 101, 2, now(), '{"customer_name":"Ray Sponsible", "customer_email":"ray.sponsible@gmail.com"}', '{"order_id": "123456"}');
+INSERT INTO T_WORKFLOW_INSTANCE(id, tenant_fk, workflow_fk, status, start_at, state, parameters)
+    VALUES ('wi-100-01', 1, 100, 2, now(), '{"customer_name":"Ray Sponsible", "customer_email":"ray.sponsible@gmail.com"}', '{"order_id": "123456"}');
 
-INSERT INTO T_WI_ACTIVITY(id, instance_fk, activity_fk, assignee_fk, approval, approver_fk, status, started_at, done_at)
-    VALUES ('wi-100-01-start',   'wi-100-01', 100, null, 0, null, 3, '2020-01-10 12:30', '2020-01-11 12:30'),
-           ('wi-100-01-working', 'wi-100-01', 101, null, 0, null, 2, '2020-01-10 12:30', null);
+INSERT INTO T_WI_ACTIVITY(id, instance_fk, activity_fk, assignee_fk, approval, status, started_at, done_at)
+    VALUES ('wi-100-01-start',   'wi-100-01', 100, null, 0, 3, '2020-01-10 12:30', '2020-01-11 12:30'),
+           ('wi-100-01-working', 'wi-100-01', 101, null, 0, 2, '2020-01-10 12:30', null);
