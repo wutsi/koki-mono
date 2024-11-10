5b43a31b89497ff9b62e9ef83cb2e88b3cdea32b
wutsi <herve.tchepannou@wutsi.com>
2024-11-10 11:52:17 -0500

    fix data

--- modules/koki-server/src/test/resources/db/test/form/ExportFormHTMLDataEndpoint.sql
+++ modules/koki-server/src/test/resources/db/test/form/ExportFormHTMLDataEndpoint.sql
@@ -11,12 +11,12 @@ INSERT INTO T_FORM(id, tenant_fk, name, title, active, content)
 INSERT INTO T_WORKFLOW(id, tenant_fk, name, description, active, parameters)
     VALUES(100, 1, 'w100', null, true, 'PARAM_1, PARAM_2');
 
-INSERT INTO T_ACTIVITY(id, workflow_fk, role_fk, name, type, requires_approval)
-    VALUES (100, 100, null, 'START',   1, false),
-           (101, 100, 11,   'WORKING', 3, false),
-           (102, 100, 10,   'SEND',    4, false),
-           (103, 100, 10,   'SUBMIT',  4, false),
-           (104, 100, null, 'STOP',    2, false);
+INSERT INTO T_ACTIVITY(id, workflow_fk, name, type, requires_approval)
+    VALUES (100, 100, 'START',   1, false),
+           (101, 100, 'WORKING', 3, false),
+           (102, 100, 'SEND',    4, false),
+           (103, 100, 'SUBMIT',  4, false),
+           (104, 100, 'STOP',    2, false);
 
 INSERT INTO T_WORKFLOW_INSTANCE(id, tenant_fk, workflow_fk, approver_fk, status, start_at, state, parameters)
     VALUES ('wi-100-01', 1, 100, 101, 2, now(), '{"customer_name":"Ray Sponsible", "customer_email":"ray.sponsible@gmail.com"}', '{"order_id": "123456"}');
