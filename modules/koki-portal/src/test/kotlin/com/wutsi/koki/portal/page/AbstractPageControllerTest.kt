54c3fa2b150bea995ae2af66fa818dbf4ffd1ce0
wutsi <herve.tchepannou@wutsi.com>
2024-11-11 10:52:48 -0500

    update coverage

---
+++ modules/koki-portal/src/test/kotlin/com/wutsi/koki/portal/page/AbstractPageControllerTest.kt
@@ -0,0 +1,211 @@
+package com.wutsi.blog.app.page
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.anyOrNull
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.whenever
+import com.wutsi.koki.error.dto.Error
+import com.wutsi.koki.error.dto.ErrorResponse
+import com.wutsi.koki.form.dto.SearchFormResponse
+import com.wutsi.koki.sdk.KokiForms
+import com.wutsi.koki.sdk.KokiWorkflowEngine
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.openqa.selenium.By
+import org.openqa.selenium.Dimension
+import org.openqa.selenium.JavascriptExecutor
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.support.ui.Select
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.http.HttpStatusCode
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.web.client.HttpClientErrorException
+import java.nio.charset.Charset
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext
+@ActiveProfiles("qa")
+abstract class AbstractPageControllerTest {
+    @LocalServerPort
+    protected val port: Int = 0
+
+    protected var url: String = ""
+
+    protected lateinit var driver: WebDriver
+
+    @MockBean
+    protected lateinit var kokiForms: KokiForms
+
+    @MockBean
+    protected lateinit var kokiWorkflowEngine: KokiWorkflowEngine
+
+    @Autowired
+    protected lateinit var objectMapper: ObjectMapper
+
+    @BeforeEach
+    fun setUp() {
+        this.url = "http://localhost:$port"
+
+        setupSelenium()
+        setupDefaultApiResponses()
+    }
+
+    private fun setupSelenium() {
+        val options = ChromeOptions()
+        options.addArguments("--disable-web-security") // To prevent CORS issues
+        options.addArguments("--lang=en")
+        options.addArguments("--allowed-ips=")
+        options.addArguments("--remote-allow-origins=*")
+        options.addArguments("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246")
+        if (System.getProperty("headless") == "true") {
+            options.addArguments("--headless")
+            options.addArguments("--no-sandbox")
+            options.addArguments("--disable-dev-shm-usage")
+        }
+
+        this.driver = ChromeDriver(options)
+        if (System.getProperty("headless") == "true") { // In headless mode, set a size that will not require vertical scrolling
+            driver.manage().window().size = Dimension(1920, 1280)
+        }
+    }
+
+    private fun setupDefaultApiResponses() {
+        doReturn(SearchFormResponse()).whenever(kokiForms)
+            .search(any(), anyOrNull(), anyOrNull(), anyOrNull(), anyOrNull(), anyOrNull())
+    }
+
+    @AfterEach
+    @Throws(Exception::class)
+    fun tearDown() {
+        driver.quit()
+    }
+
+    protected fun navigateTo(path: String) {
+        driver.get("http://localhost:$port$path")
+    }
+
+    protected fun createHttpClientErrorException(statusCode: Int, errorCode: String):HttpClientErrorException{
+        val charset = Charset.defaultCharset()
+        val response = ErrorResponse(
+            error = Error(code=errorCode)
+        )
+        return HttpClientErrorException(
+            HttpStatusCode.valueOf(statusCode),
+            "Error",
+            objectMapper.writeValueAsString(response).toByteArray(charset),
+            charset
+        )
+    }
+
+
+    protected fun assertCurrentPageIs(page: String) {
+        assertEquals(page, driver.findElement(By.cssSelector("meta[name=wutsi\\:page_name]"))?.getAttribute("content"))
+    }
+
+    protected fun assertElementNotPresent(selector: String) {
+        assertTrue(driver.findElements(By.cssSelector(selector)).size == 0)
+    }
+
+    protected fun assertElementPresent(selector: String) {
+        assertTrue(driver.findElements(By.cssSelector(selector)).size > 0)
+    }
+
+    protected fun assertElementText(selector: String, text: String?) {
+        assertEquals(text, driver.findElement(By.cssSelector(selector)).text)
+    }
+
+    protected fun assertElementTextContains(selector: String, text: String) {
+        assertTrue(driver.findElement(By.cssSelector(selector)).text.contains(text))
+    }
+
+    protected fun assertElementCount(selector: String, count: Int) {
+        assertEquals(count, driver.findElements(By.cssSelector(selector)).size)
+    }
+
+    protected fun assertElementNotVisible(selector: String) {
+        assertEquals("none", driver.findElement(By.cssSelector(selector)).getCssValue("display"))
+    }
+
+    protected fun assertElementVisible(selector: String) {
+        assertFalse("none".equals(driver.findElement(By.cssSelector(selector)).getCssValue("display")))
+    }
+
+    protected fun assertElementAttribute(selector: String, name: String, value: String?) {
+        if (value == null) {
+            assertNull(driver.findElement(By.cssSelector(selector)).getAttribute(name))
+        } else {
+            assertEquals(value, driver.findElement(By.cssSelector(selector)).getAttribute(name))
+        }
+    }
+
+    protected fun assertElementAttributeNull(selector: String, name: String) {
+        val value = driver.findElement(By.cssSelector(selector)).getAttribute(name)
+        assertTrue(value.isNullOrEmpty())
+    }
+
+    protected fun assertElementAttributeStartsWith(selector: String, name: String, value: String) {
+        assertTrue(driver.findElement(By.cssSelector(selector)).getAttribute(name).startsWith(value))
+    }
+
+    protected fun assertElementAttributeEndsWith(selector: String, name: String, value: String) {
+        assertTrue(driver.findElement(By.cssSelector(selector)).getAttribute(name).endsWith(value))
+    }
+
+    protected fun assertElementAttributeContains(selector: String, name: String, value: String) {
+        assertTrue(driver.findElement(By.cssSelector(selector)).getAttribute(name).contains(value))
+    }
+
+    protected fun assertElementHasClass(selector: String, value: String) {
+        assertTrue(driver.findElement(By.cssSelector(selector)).getAttribute("class").contains(value))
+    }
+
+    protected fun assertElementHasNotClass(selector: String, value: String) {
+        assertFalse(driver.findElement(By.cssSelector(selector)).getAttribute("class").contains(value))
+    }
+
+    protected fun click(selector: String, delayMillis: Long? = null) {
+        driver.findElement(By.cssSelector(selector)).click()
+        delayMillis?.let { Thread.sleep(delayMillis) }
+    }
+
+    protected fun scrollToBottom() {
+        val js = driver as JavascriptExecutor
+        js.executeScript("window.scrollBy(0,document.body.scrollHeight)")
+        Thread.sleep(1000)
+    }
+
+    protected fun scrollToMiddle() {
+        val js = driver as JavascriptExecutor
+        js.executeScript("window.scrollBy(0,document.body.scrollHeight/2)")
+        Thread.sleep(1000)
+    }
+
+    protected fun scroll(percent: Double) {
+        val js = driver as JavascriptExecutor
+        js.executeScript("window.scrollBy(0,document.body.scrollHeight*$percent)")
+        Thread.sleep(1000)
+    }
+
+    protected fun input(selector: String, value: String) {
+        val by = By.cssSelector(selector)
+        driver.findElement(by).clear()
+        driver.findElement(by).sendKeys(value)
+    }
+
+    protected fun select(selector: String, index: Int) {
+        val by = By.cssSelector(selector)
+        val select = Select(driver.findElement(by))
+        select.selectByIndex(index)
+    }
+}
