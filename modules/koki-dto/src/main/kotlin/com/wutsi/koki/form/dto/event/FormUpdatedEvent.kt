5fcf53df99e8cee1600462d94b15fd39105bef24
wutsi <herve.tchepannou@wutsi.com>
2024-11-14 07:57:59 -0500

    update events

--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/form/dto/event/FormUpdatedEvent.kt
+++ modules/koki-dto/src/main/kotlin/com/wutsi/koki/form/dto/event/FormUpdatedEvent.kt
@@ -1,6 +1,6 @@
 package com.wutsi.koki.form.event
 
-data class FormSubmittedEvent(
+data class FormUpdatedEvent(
     val formId: String = "",
     val formDataId: String = "",
     val timestamp: Long = System.currentTimeMillis(),
