3cffb3048ce7f43233eca613f1f91ce53c36e57b
wutsi <herve.tchepannou@wutsi.com>
2024-11-15 11:20:40 -0500

    update schema

--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/workflow/dto/ApproveActivityInstanceRequest.kt
+++ modules/koki-dto/src/main/kotlin/com/wutsi/koki/workflow/dto/ApproveActivityInstanceRequest.kt
@@ -3,5 +3,4 @@ package com.wutsi.koki.workflow.dto
 data class ApproveActivityInstanceRequest(
     val status: ApprovalStatus = ApprovalStatus.UNKNOWN,
     val comment: String? = null,
-    val approverUserId: Long = -1,
 )
