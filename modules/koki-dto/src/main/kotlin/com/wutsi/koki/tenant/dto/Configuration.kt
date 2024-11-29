bfddc05e3ad01a2dc37a2ded1690e88a529a9139
wutsi <herve.tchepannou@wutsi.com>
2024-11-29 16:53:15 -0500

    update configuration schema

--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/tenant/dto/Configuration.kt
+++ modules/koki-dto/src/main/kotlin/com/wutsi/koki/tenant/dto/Configuration.kt
@@ -3,8 +3,7 @@ package com.wutsi.koki.tenant.dto
 import java.util.Date
 
 data class Configuration(
-    val id: Long = -1,
-    val attribute: Attribute = Attribute(),
+    val name: String = "",
     val value: String? = null,
     val createdAt: Date = Date(),
     val modifiedAt: Date = Date(),
