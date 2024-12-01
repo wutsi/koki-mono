9794f6618b3b956dd6dacd04141003f3bbe874a3
wutsi <herve.tchepannou@wutsi.com>
2024-12-01 04:33:12 -0500

    fix klint  issue

--- modules/koki-server/src/main/kotlin/com/wutsi/koki/workflow/server/endpoint/SearchWorkflowEndpoint.kt
+++ modules/koki-server/src/main/kotlin/com/wutsi/koki/workflow/server/endpoint/SearchWorkflowEndpoint.kt
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import kotlin.math.min
 
 @RestController
 @RequestMapping
