cf70713bc53dadd304df317cf4f1f802fb67532d
wutsi <herve.tchepannou@wutsi.com>
2024-12-14 20:52:02 -0500

    fix test

--- modules/koki-server/src/test/kotlin/com/wutsi/koki/script/server/endpoint/RunScriptEndpointTest.kt
+++ modules/koki-server/src/test/kotlin/com/wutsi/koki/script/server/endpoint/RunScriptEndpointTest.kt
@@ -1,6 +1,8 @@
 package com.wutsi.koki.script.server.endpoint
 
 import com.wutsi.koki.TenantAwareEndpointTest
+import com.wutsi.koki.error.dto.ErrorCode
+import com.wutsi.koki.error.dto.ErrorResponse
 import com.wutsi.koki.script.dto.Language
 import com.wutsi.koki.script.dto.RunScriptRequest
 import com.wutsi.koki.script.dto.RunScriptResponse
@@ -10,7 +12,7 @@ import org.springframework.http.HttpStatus
 
 class RunScriptEndpointTest : TenantAwareEndpointTest() {
     @Test
-    fun execute() {
+    fun run() {
         val request = RunScriptRequest(
             language = Language.JAVASCRIPT,
             parameters = mapOf(
@@ -19,7 +21,7 @@ class RunScriptEndpointTest : TenantAwareEndpointTest() {
             ),
             code = """
                 console.log("Hello");
-                return a - b;
+                a - b;
             """.trimIndent()
         )
         val response = rest.postForEntity("/v1/scripts/run", request, RunScriptResponse::class.java)
@@ -28,5 +30,27 @@ class RunScriptEndpointTest : TenantAwareEndpointTest() {
 
         assertEquals(10, response.body!!.bindings["return"])
         assertEquals("Hello\n", response.body!!.console)
+    }
+
+    @Test
+    fun error() {
+        val request = RunScriptRequest(
+            language = Language.JAVASCRIPT,
+            parameters = mapOf(
+                "a" to 11,
+                "b" to 1,
+            ),
+            code = """
+                console.log("Hello");
+                f();
+                a - b;
+            """.trimIndent()
+        )
+        val response = rest.postForEntity("/v1/scripts/run", request, ErrorResponse::class.java)
+
+        assertEquals(HttpStatus.CONFLICT, response.statusCode)
+
+        assertEquals(ErrorCode.SCRIPT_EXECUTION_FAILED, response.body!!.error.code)
+        assertEquals("Hello\n", response.body!!.error.data?.get("console"))
     }
 }
