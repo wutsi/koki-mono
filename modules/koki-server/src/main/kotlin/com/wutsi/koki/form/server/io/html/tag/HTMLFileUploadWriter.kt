510086b18a71ae95c0ab47a7d6df5a7c28c37523
wutsi <herve.tchepannou@wutsi.com>
2024-12-21 04:47:27 -0500

    shoe file icon

--- modules/koki-server/src/main/kotlin/com/wutsi/koki/form/server/io/html/tag/HTMLFileUploadWriter.kt
+++ modules/koki-server/src/main/kotlin/com/wutsi/koki/form/server/io/html/tag/HTMLFileUploadWriter.kt
@@ -1,6 +1,7 @@
 package com.wutsi.koki.form.server.generator.html
 
 import com.wutsi.koki.form.dto.FormElement
+import org.apache.commons.io.FilenameUtils
 import org.apache.commons.text.StringEscapeUtils
 import java.io.StringWriter
 
@@ -14,14 +15,14 @@ class HTMLFileUploadWriter : AbstractHTMLImputElementWriter() {
         if (!value.isNullOrEmpty()) {
             writer.write(" value='$value'")
         }
-        if (element.required) {
+        if (element.required && !readOnly) {
             writer.write(" required")
         }
         writer.write("/>\n")
 
         if (!readOnly) {
             writer.write("  <BUTTON type='button' class='btn-upload' rel='${element.name}'>Upload File</BUTTON>\n")
-            writer.write("  <INPUT type='file' name='${element.name}-file' rel='${element.name}' data-upload-url='${context.uploadUrl}'/>\n")
+            writer.write("  <INPUT type='file' id='${element.name}-file' name='${element.name}-file' rel='${element.name}' data-upload-url='${context.uploadUrl}'/>\n")
         }
 
         writer.write("  <SPAN data-name='${element.name}-filename'>\n")
@@ -29,7 +30,9 @@ class HTMLFileUploadWriter : AbstractHTMLImputElementWriter() {
             val file = context.fileResolver.resolve(value, context.tenantId)
             if (file != null) {
                 val filename = StringEscapeUtils.escapeHtml4((file.name))
-                writer.write("    <A class='filename' href='${context.downloadUrl}/$value/$filename'>$filename</A>\n")
+                val ext = FilenameUtils.getExtension(filename).lowercase()
+
+                writer.write("    <A class='filename' href='${context.downloadUrl}/$value/$filename'><SPAN class='fiv-viv fiv-icon-$ext'></SPAN>&nbsp;$filename</A>\n")
                 if (!readOnly) {
                     writer.write("    <button class='btn-close' type='button' name='${element.name}-close' rel='${element.name}'></button>\n")
                 }
