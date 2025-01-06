4ecb183a129920dc397aa7caf6fcbb8fca306bba
wutsi <herve.tchepannou@wutsi.com>
2025-01-06 01:20:31 -0500

    fix test

--- modules/koki-server/src/test/resources/db/test/contact/CreateContactEndpoint.sql
+++ modules/koki-server/src/test/resources/db/test/contact/CreateContactEndpoint.sql
@@ -2,6 +2,6 @@ INSERT INTO T_CONTACT_TYPE(id, tenant_fk, name, active)
     VALUES (100, 1, 'primary', true),
            (200, 2, 'aa', false);
 
-INSERT INTO T_ACCOUNT(id, tenant_fk, account_type_fk, name)
-    VALUES(1000, 1, 100,  'Inc'),
-          (2000, 2, null, 'Inc');
+INSERT INTO T_ACCOUNT(id, tenant_fk, name)
+    VALUES(1000, 1, 'Inc'),
+          (2000, 2, 'Inc');
