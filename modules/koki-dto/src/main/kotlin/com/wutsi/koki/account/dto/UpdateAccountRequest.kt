4f87fa65975876b18f9f7ee5c1b456fd774c13ee
wutsi <herve.tchepannou@wutsi.com>
2025-01-28 13:57:04 -0500

    update schemas

--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/account/dto/UpdateAccountRequest.kt
+++ modules/koki-dto/src/main/kotlin/com/wutsi/koki/account/dto/UpdateAccountRequest.kt
@@ -2,15 +2,18 @@ package com.wutsi.koki.account.dto
 
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.Size
 
 data class UpdateAccountRequest(
-    @get:NotEmpty val name: String = "",
     val accountTypeId: Long? = null,
-    val phone: String? = null,
-    val mobile: String? = null,
-    @get:Email val email: String? = null,
+
+    @get:NotEmpty @get:Size(max = 100) val name: String = "",
+    @get:Size(max = 30) val phone: String? = null,
+    @get:Size(max = 30) val mobile: String? = null,
+    @get:Email @get:Size(max = 255) val email: String? = null,
+    @get:Size(max = 2) val language: String? = null,
+
     val website: String? = null,
-    val language: String? = null,
     val description: String? = null,
     val attributes: Map<Long, String> = emptyMap(),
     val managedById: Long? = null,
--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/contact/dto/CreateContactRequest.kt
+++ modules/koki-dto/src/main/kotlin/com/wutsi/koki/contact/dto/CreateContactRequest.kt
@@ -1,17 +1,20 @@
 package com.wutsi.koki.contact.dto
 
 import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.Size
 
 data class CreateContactRequest(
     val accountId: Long? = null,
     val contactTypeId: Long? = null,
-    val salutations: String? = null,
-    @get:NotEmpty() val firstName: String = "",
-    @get:NotEmpty() val lastName: String = "",
-    val phone: String? = null,
-    val mobile: String? = null,
-    val email: String? = null,
+
+    @get:Size(max = 10) val salutations: String? = null,
+    @get:NotEmpty() @get:Size(max = 100) val firstName: String = "",
+    @get:NotEmpty() @get:Size(max = 100) val lastName: String = "",
+    @get:Size(max = 30) val phone: String? = null,
+    @get:Size(max = 30) val mobile: String? = null,
+    @get:Size(max = 255) val email: String? = null,
+    @get:Size(max = 100) val profession: String? = null,
+    @get:Size(max = 100) val employer: String? = null,
+
     val gender: Gender = Gender.UNKNOWN,
-    val profession: String? = null,
-    val employer: String? = null,
 )
