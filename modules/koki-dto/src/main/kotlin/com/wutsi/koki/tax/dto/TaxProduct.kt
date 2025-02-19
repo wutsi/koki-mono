47e6a0a7ff5b4b628ba20400375898cf20551f52
wutsi <herve.tchepannou@wutsi.com>
2025-02-18 20:03:25 -0500

    update schema

--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/tax/dto/TaxProduct.kt
+++ modules/koki-dto/src/main/kotlin/com/wutsi/koki/tax/dto/TaxProduct.kt
@@ -4,8 +4,10 @@ data class TaxProduct(
     val id: Long = -1,
     val taxId: Long = -1,
     val productId: Long = -1,
+    val unitPriceId: Long = -1,
     val description: String? = null,
     val quantity: Int = 1,
     val unitPrice: Double = 0.0,
     val subTotal: Double = 0.0,
+    val currency: String = "",
 )
