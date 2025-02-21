a0a89ea17c179aecd15affb5e84783132158393d
wutsi <herve.tchepannou@wutsi.com>
2025-02-21 13:42:22 -0500

    update business api

--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/invoicing/dto/InvoiceItem.kt
+++ modules/koki-dto/src/main/kotlin/com/wutsi/koki/invoicing/dto/InvoiceItem.kt
@@ -3,6 +3,7 @@ package com.wutsi.koki.invoicing.dto
 data class InvoiceItem(
     val id: Long = -1,
     val productId: Long = -1,
+    val unitPriceId: Long = -1,
     val description: String = "",
     val quantity: Int = -1,
     val unitPrice: Double = 0.0,
---
+++ modules/koki-dto/src/main/kotlin/com/wutsi/koki/tenant/dto/GetBusinessResponse.kt
@@ -0,0 +1,5 @@
+package com.wutsi.koki.tenant.dto
+
+data class GetBusinessResponse(
+    val business: Business = Business()
+)
