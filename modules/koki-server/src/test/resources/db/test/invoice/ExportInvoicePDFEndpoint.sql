a60fd3784c4d92c1946f1f215b797727a957d68f
wutsi <herve.tchepannou@wutsi.com>
2025-03-02 20:38:15 -0500

    fix test

--- modules/koki-server/src/test/resources/db/test/invoice/ExportInvoicePDFEndpoint.sql
+++ modules/koki-server/src/test/resources/db/test/invoice/ExportInvoicePDFEndpoint.sql
@@ -13,6 +13,23 @@ INSERT INTO T_UNIT(id, name)
            (141, 'Treatment'),
            (142, 'Lesson');
 
+INSERT INTO T_LOCATION (id, type, name, ascii_name, country)
+    VALUES (101, 2, 'Alberta', 'Alberta', 'CA'),
+           (102, 2, 'British Columbia', 'British Columbia', 'CA'),
+           (103, 2, 'Manitoba', 'Manitoba', 'CA'),
+           (104, 2, 'New Brunswick', 'New Brunswick', 'CA'),
+           (105, 2, 'Newfoundland and Labrador', 'Newfoundland and Labrador', 'CA'),
+           (106, 2, 'Northwest Territories', 'Northwest Territories', 'CA'),
+           (107, 2, 'Nova Scotia', 'Nova Scotia', 'CA'),
+           (108, 2, 'Nunavut', 'Nunavut', 'CA'),
+           (109, 2, 'Ontario', 'Ontario', 'CA'),
+           (110, 2, 'Prince Edward Island', 'Prince Edward Island', 'CA'),
+           (111, 2, 'Quebec', 'Quebec', 'CA'),
+           (112, 2, 'Saskatchewan', 'Saskatchewan', 'CA'),
+           (113, 2, 'Yukon', 'Yukon', 'CA'),
+           (200, 1, 'Cameroon', 'Cameron', 'C<')
+;
+
 INSERT INTO T_JURIDICTION(id, country, state_fk)
     VALUES(1000, 'CA', 101),
           (1008, 'CA', 109),
