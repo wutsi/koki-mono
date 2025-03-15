7723c7f585394fc61a5901789fa7233f37c25168
wutsi <herve.tchepannou@wutsi.com>
2025-03-14 20:44:56 -0400

    update schema

--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/payment/dto/PrepareCheckoutResponse.kt
+++ modules/koki-dto/src/main/kotlin/com/wutsi/koki/payment/dto/PrepareCheckoutResponse.kt
@@ -2,6 +2,6 @@ package com.wutsi.koki.payment.dto
 
 data class PrepareCheckoutResponse(
     val transactionId: String = "",
-    val redirectUrl: String = "",
+    val redirectUrl: String? = null,
     val status: TransactionStatus = TransactionStatus.UNKNOWN,
 )
