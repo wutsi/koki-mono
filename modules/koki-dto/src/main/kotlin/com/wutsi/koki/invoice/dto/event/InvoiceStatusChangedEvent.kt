9bd34ea56688d754087ad3a2208cbd9d10be129b
wutsi <herve.tchepannou@wutsi.com>
2025-03-17 13:03:05 -0400

    change invoiceId type

--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/invoice/dto/event/InvoiceStatusChangedEvent.kt
+++ modules/koki-dto/src/main/kotlin/com/wutsi/koki/invoice/dto/event/InvoiceStatusChangedEvent.kt
@@ -3,7 +3,7 @@ package com.wutsi.koki.invoice.dto.event
 import com.wutsi.koki.invoice.dto.InvoiceStatus
 
 data class InvoiceStatusChangedEvent(
-    val invoiceId: Long = -1,
+    val invoiceId: String = "",
     val tenantId: Long = -1,
     val status: InvoiceStatus = InvoiceStatus.UNKNOWN,
     val timestamp: Long = System.currentTimeMillis()
