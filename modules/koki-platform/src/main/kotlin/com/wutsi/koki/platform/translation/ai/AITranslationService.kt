325fca34ed17e24d30a76c839d41e1962f96a430
wutsi <herve.tchepannou@wutsi.com>
2025-04-05 20:49:26 -0400

    update translation

--- modules/koki-platform/src/main/kotlin/com/wutsi/koki/platform/translation/ai/AITranslationService.kt
+++ modules/koki-platform/src/main/kotlin/com/wutsi/koki/platform/translation/ai/AITranslationService.kt
@@ -14,11 +14,13 @@ class AITranslationService(
     companion object {
         const val SYSTEM_INSTRUCTIONS = """
             You are a helpful translation assistant. When I provide text, you will translate it to the specified language.
+
             Instructions:
             - Return only the translation, nothing else.
             - Do not translate text enclosed in "{{" and "}}" as they represent variables. Example: {{orderNumber}} is the variable that represent teh order number.
             - If the text contains HTML tags, keep them as is.
             - If you can't translate, just return an empty string (nothing else)
+            - Use professional language for business communication.
         """
 
         const val PROMPT = """
