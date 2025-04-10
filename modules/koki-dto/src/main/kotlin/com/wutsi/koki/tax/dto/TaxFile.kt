59c04536fca9efb95e4f03943daa73a3a2ae117c
wutsi <herve.tchepannou@wutsi.com>
2025-04-10 15:56:28 -0400

    add tax-id

--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/tax/dto/TaxFile.kt
+++ modules/koki-dto/src/main/kotlin/com/wutsi/koki/tax/dto/TaxFile.kt
@@ -4,6 +4,7 @@ import java.util.Date
 
 data class TaxFile(
     val fileId: Long = -1,
+    val taxId: Long = -1,
     val data: TaxFileData = TaxFileData(),
     val createdAt: Date = Date(),
     val modifiedAt: Date = Date()
