8b7ae7ee17193b95fe28b38da95bb5676f457a0d
wutsi <herve.tchepannou@wutsi.com>
2025-05-04 00:22:23 -0400

    add room sdk

--- modules/koki-sdk/src/main/kotlin/com/wutsi/koki/sdk/KokiRooms.kt
+++ modules/koki-sdk/src/main/kotlin/com/wutsi/koki/sdk/KokiRooms.kt
@@ -1,119 +1,63 @@
 package com.wutsi.koki.sdk
 
-import com.wutsi.koki.account.dto.CreateAccountRequest
-import com.wutsi.koki.account.dto.CreateAccountResponse
-import com.wutsi.koki.account.dto.CreateInvitationRequest
-import com.wutsi.koki.account.dto.CreateInvitationResponse
-import com.wutsi.koki.account.dto.CreateUserRequest
-import com.wutsi.koki.account.dto.CreateUserResponse
-import com.wutsi.koki.account.dto.GetAccountResponse
-import com.wutsi.koki.account.dto.GetAttributeResponse
-import com.wutsi.koki.account.dto.GetInvitationResponse
-import com.wutsi.koki.account.dto.SearchAccountResponse
-import com.wutsi.koki.account.dto.SearchAttributeResponse
-import com.wutsi.koki.account.dto.UpdateAccountRequest
-import com.wutsi.koki.common.dto.ImportResponse
+import com.wutsi.koki.room.dto.CreateRoomRequest
+import com.wutsi.koki.room.dto.CreateRoomResponse
+import com.wutsi.koki.room.dto.GetRoomResponse
+import com.wutsi.koki.room.dto.RoomStatus
+import com.wutsi.koki.room.dto.RoomType
+import com.wutsi.koki.room.dto.SearchRoomResponse
+import com.wutsi.koki.room.dto.UpdateRoomRequest
 import org.springframework.web.client.RestTemplate
-import org.springframework.web.multipart.MultipartFile
 
-class KokiAccounts(
+class KokiRooms(
     private val urlBuilder: URLBuilder,
     rest: RestTemplate,
 ) : AbstractKokiModule(rest) {
     companion object {
-        private const val ACCOUNT_PATH_PREFIX = "/v1/accounts"
-        private const val ATTRIBUTE_PATH_PREFIX = "/v1/attributes"
-        private const val INVITATION_PATH_PREFIX = "/v1/invitations"
+        private const val ROOM_PATH_PREFIX = "/v1/rooms"
     }
 
-    fun create(request: CreateAccountRequest): CreateAccountResponse {
-        val url = urlBuilder.build(ACCOUNT_PATH_PREFIX)
-        return rest.postForEntity(url, request, CreateAccountResponse::class.java).body
+    fun create(request: CreateRoomRequest): CreateRoomResponse {
+        val url = urlBuilder.build(ROOM_PATH_PREFIX)
+        return rest.postForEntity(url, request, CreateRoomResponse::class.java).body
     }
 
-    fun update(id: Long, request: UpdateAccountRequest) {
-        val url = urlBuilder.build("$ACCOUNT_PATH_PREFIX/$id")
+    fun update(id: Long, request: UpdateRoomRequest) {
+        val url = urlBuilder.build("$ROOM_PATH_PREFIX/$id")
         rest.postForEntity(url, request, Any::class.java)
     }
 
     fun delete(id: Long) {
-        val url = urlBuilder.build("$ACCOUNT_PATH_PREFIX/$id")
+        val url = urlBuilder.build("$ROOM_PATH_PREFIX/$id")
         rest.delete(url)
     }
 
-    fun account(id: Long): GetAccountResponse {
-        val url = urlBuilder.build("$ACCOUNT_PATH_PREFIX/$id")
-        return rest.getForEntity(url, GetAccountResponse::class.java).body
+    fun room(id: Long): GetRoomResponse {
+        val url = urlBuilder.build("$ROOM_PATH_PREFIX/$id")
+        return rest.getForEntity(url, GetRoomResponse::class.java).body
     }
 
-    fun accounts(
-        keyword: String?,
+    fun rooms(
         ids: List<Long>,
-        accountTypeIds: List<Long>,
-        managedByIds: List<Long>,
-        createdByIds: List<Long>,
-        userIds: List<Long>,
+        cityId: Long?,
+        status: RoomStatus?,
+        type: RoomType?,
+        totalGuests: Int?,
         limit: Int,
         offset: Int,
-    ): SearchAccountResponse {
+    ): SearchRoomResponse {
         val url = urlBuilder.build(
-            ACCOUNT_PATH_PREFIX,
+            ROOM_PATH_PREFIX,
             mapOf(
-                "q" to keyword,
                 "id" to ids,
-                "account-type-id" to accountTypeIds,
-                "managed-by-id" to managedByIds,
-                "created-by-id" to createdByIds,
-                "user-id" to userIds,
+                "city-id" to cityId,
+                "status" to status,
+                "type" to type,
+                "total-guests" to totalGuests,
                 "limit" to limit,
                 "offset" to offset,
             )
         )
-        return rest.getForEntity(url, SearchAccountResponse::class.java).body
-    }
-
-    fun attribute(id: Long): GetAttributeResponse {
-        val url = urlBuilder.build("$ATTRIBUTE_PATH_PREFIX/$id")
-        return rest.getForEntity(url, GetAttributeResponse::class.java).body
-    }
-
-    fun attributes(
-        ids: List<Long>,
-        names: List<String>,
-        active: Boolean?,
-        limit: Int,
-        offset: Int,
-    ): SearchAttributeResponse {
-        val url = urlBuilder.build(
-            ATTRIBUTE_PATH_PREFIX,
-            mapOf(
-                "id" to ids,
-                "name" to names,
-                "active" to active,
-                "limit" to limit,
-                "offset" to offset,
-            )
-        )
-        return rest.getForEntity(url, SearchAttributeResponse::class.java).body
-    }
-
-    fun uploadAttributes(file: MultipartFile): ImportResponse {
-        val url = urlBuilder.build("$ATTRIBUTE_PATH_PREFIX/csv")
-        return upload(url, file, ImportResponse::class.java)
-    }
-
-    fun createUser(accountId: Long, request: CreateUserRequest): CreateUserResponse {
-        val url = urlBuilder.build("$ACCOUNT_PATH_PREFIX/$accountId/user")
-        return rest.postForEntity(url, request, CreateUserResponse::class.java).body
-    }
-
-    fun createInvitation(request: CreateInvitationRequest): CreateInvitationResponse {
-        val url = urlBuilder.build(INVITATION_PATH_PREFIX)
-        return rest.postForEntity(url, request, CreateInvitationResponse::class.java).body
-    }
-
-    fun invitation(id: String): GetInvitationResponse {
-        val url = urlBuilder.build("$INVITATION_PATH_PREFIX/$id")
-        return rest.getForEntity(url, GetInvitationResponse::class.java).body
+        return rest.getForEntity(url, SearchRoomResponse::class.java).body
     }
 }
