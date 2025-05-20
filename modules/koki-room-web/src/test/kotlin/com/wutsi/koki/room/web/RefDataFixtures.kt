11868162a9385281f721f8b92eb5867920cd1951
wutsi <herve.tchepannou@wutsi.com>
2025-05-20 07:13:09 -0400

    add test

---
+++ modules/koki-room-web/src/test/kotlin/com/wutsi/koki/room/web/RefDataFixtures.kt
@@ -0,0 +1,149 @@
+package com.wutsi.koki.room.web
+
+import com.wutsi.koki.refdata.dto.Amenity
+import com.wutsi.koki.refdata.dto.Category
+import com.wutsi.koki.refdata.dto.CategoryType
+import com.wutsi.koki.refdata.dto.Juridiction
+import com.wutsi.koki.refdata.dto.Location
+import com.wutsi.koki.refdata.dto.LocationType
+import com.wutsi.koki.refdata.dto.SalesTax
+import com.wutsi.koki.refdata.dto.Unit
+import org.openqa.selenium.By.id
+
+object RefDataFixtures {
+    // Units
+    val units = listOf(
+        Unit(id = 100, name = "Hours"),
+        Unit(id = 110, name = "Days"),
+        Unit(id = 120, name = "Month"),
+        Unit(id = 130, name = "Visit"),
+    )
+
+    // Location
+    val locations = listOf(
+        Location(id = 100, type = LocationType.STATE, name = "Quebec", country = "CA"),
+        Location(id = 200, type = LocationType.STATE, name = "Ontario", country = "CA"),
+        Location(
+            id = 110,
+            type = LocationType.CITY,
+            name = "Montreal",
+            parentId = 100,
+            country = "CA",
+            latitude = 45.508888,
+            longitude = -73.561668
+        ),
+        Location(
+            id = 120,
+            type = LocationType.CITY,
+            name = "Saint Isidore",
+            parentId = 100,
+            country = "CA",
+            latitude = 45.300102,
+            longitude = -73.680274
+        ),
+        Location(
+            id = 210,
+            type = LocationType.CITY,
+            name = "Toronto",
+            parentId = 200,
+            country = "CA",
+            latitude = 43.651070,
+            longitude = -79.347015
+        ),
+        Location(
+            id = 111,
+            type = LocationType.NEIGHBORHOOD,
+            name = "Centre-Ville",
+            parentId = 110,
+            country = "CA",
+            latitude = 45.508888,
+            longitude = -73.561668
+        ),
+        Location(
+            id = 112,
+            type = LocationType.NEIGHBORHOOD,
+            name = "Ahunsic",
+            parentId = 110,
+            country = "CA",
+            latitude = 45.5330,
+            longitude = -73.7170
+        ),
+        Location(
+            id = 113,
+            type = LocationType.NEIGHBORHOOD,
+            name = "Mont-Royal",
+            parentId = 110,
+            country = "CA",
+            latitude = 45.516109,
+            longitude = -73.643059
+        ),
+        Location(id = 114, type = LocationType.NEIGHBORHOOD, name = "Cote des Neiges", parentId = 110, country = "CA"),
+    )
+    val cities = listOf(
+        Location(id = 110, type = LocationType.CITY, name = "Montreal", parentId = 100, country = "CA"),
+        Location(id = 120, type = LocationType.CITY, name = "Saint Isidore", parentId = 100, country = "CA"),
+        Location(id = 210, type = LocationType.CITY, name = "Toronto", parentId = 200, country = "CA"),
+    )
+    val neighborhoods = listOf(
+        Location(id = 111, type = LocationType.NEIGHBORHOOD, name = "Centre-Ville", parentId = 110, country = "CA"),
+        Location(id = 112, type = LocationType.NEIGHBORHOOD, name = "Ahunsic", parentId = 110, country = "CA"),
+        Location(id = 113, type = LocationType.NEIGHBORHOOD, name = "Mont-Royal", parentId = 110, country = "CA"),
+        Location(id = 114, type = LocationType.NEIGHBORHOOD, name = "Cote des Neiges", parentId = 110, country = "CA"),
+    )
+
+    // Categories
+    val categories = listOf(
+        Category(id = 1000, type = CategoryType.SERVICE, name = "Automotive", longName = "Automotive"),
+        Category(id = 1001, type = CategoryType.SERVICE, name = "Repair", longName = "Automotive > Repair"),
+        Category(
+            id = 1002,
+            type = CategoryType.SERVICE,
+            name = "Engine Repair",
+            longName = "Automotive > Repair > Engine Repair"
+        ),
+        Category(
+            id = 1002,
+            type = CategoryType.SERVICE,
+            name = "Brake Repair",
+            longName = "Automotive > Repair > Brake Repair"
+        ),
+    )
+
+    // Juridiction
+    val juridictions = listOf(
+        Juridiction(id = 100, stateId = null, country = "CA"),
+        Juridiction(id = 101, stateId = locations[0].id, country = "CA"),
+        Juridiction(id = 102, stateId = locations[1].id, country = "CA"),
+        Juridiction(id = 237, stateId = null, country = "CM"),
+    )
+
+    // Sales Taxes
+    val salesTaxes = listOf(
+        SalesTax(id = 10000, name = "GST", rate = 5.0, juridictionId = 100),
+        SalesTax(id = 10100, name = "GST", rate = 5.0, juridictionId = 101),
+        SalesTax(id = 10101, name = "PST", rate = 9.975, juridictionId = 101),
+        SalesTax(id = 10200, name = "HST", rate = 13.0, juridictionId = 102),
+    )
+
+    // Amenities
+    private var amenityIdCounter = System.currentTimeMillis()
+    val amenities = categories.flatMap { category ->
+        listOf(
+            Amenity(
+                id = amenityIdCounter++,
+                categoryId = category.id,
+                name = "Amenity #$amenityIdCounter",
+            ),
+            Amenity(
+                id = amenityIdCounter++,
+                categoryId = category.id,
+                name = "Amenity #$amenityIdCounter",
+            ),
+            Amenity(
+                id = amenityIdCounter++,
+                categoryId = category.id,
+                name = "Amenity #$amenityIdCounter",
+            ),
+        )
+    }
+}
---
+++ modules/koki-room-web/src/test/kotlin/com/wutsi/koki/room/web/TenantFixtures.kt
@@ -0,0 +1,112 @@
+package com.wutsi.koki.room.web
+
+import com.wutsi.koki.common.dto.ObjectType
+import com.wutsi.koki.platform.ai.llm.LLMType
+import com.wutsi.koki.platform.translation.TranslationProvider
+import com.wutsi.koki.tenant.dto.ConfigurationName
+import com.wutsi.koki.tenant.dto.Tenant
+import com.wutsi.koki.tenant.dto.TenantStatus
+import com.wutsi.koki.tenant.dto.Type
+import com.wutsi.koki.tenant.dto.TypeSummary
+
+object TenantFixtures {
+    val config = mapOf(
+        ConfigurationName.AI_PROVIDER to LLMType.KOKI.name,
+        ConfigurationName.AI_PROVIDER_GEMINI_MODEL to "gemini-2.0-flash",
+        ConfigurationName.AI_PROVIDER_GEMINI_API_KEY to "kk-1203923-4390r-erf00943",
+
+        ConfigurationName.INVOICE_DUE_DAYS to "30",
+        ConfigurationName.INVOICE_START_NUMBER to "1550",
+        ConfigurationName.INVOICE_EMAIL_ENABLED to "1",
+        ConfigurationName.INVOICE_EMAIL_SUBJECT to "Invoice #{{invoiceNumber}}",
+        ConfigurationName.INVOICE_EMAIL_BODY to "You have a new invoice!",
+
+        ConfigurationName.PAYMENT_EMAIL_ENABLED to "1",
+        ConfigurationName.PAYMENT_EMAIL_SUBJECT to "Thank you for your payment - Invoice #{{invoiceNumber}}",
+        ConfigurationName.PAYMENT_EMAIL_BODY to "Thank you!",
+        ConfigurationName.PAYMENT_METHOD_BANK_ENABLED to "1",
+        ConfigurationName.PAYMENT_METHOD_CASH_ENABLED to "1",
+        ConfigurationName.PAYMENT_METHOD_CASH_INSTRUCTIONS to "You can send your email at address:\n3030 Linton",
+        ConfigurationName.PAYMENT_METHOD_CHECK_ENABLED to "1",
+        ConfigurationName.PAYMENT_METHOD_INTERAC_ENABLED to "1",
+        ConfigurationName.PAYMENT_METHOD_CREDIT_CARD_ENABLED to "1",
+        ConfigurationName.PAYMENT_METHOD_CREDIT_CARD_GATEWAY to "STRIPE",
+        ConfigurationName.PAYMENT_METHOD_CREDIT_CARD_GATEWAY_STRIPE_API_KEY to "SRP.1234567890",
+        ConfigurationName.PAYMENT_METHOD_PAYPAL_ENABLED to "1",
+        ConfigurationName.PAYMENT_METHOD_PAYPAL_CLIENT_ID to "PP.1234567890",
+        ConfigurationName.PAYMENT_METHOD_MOBILE_ENABLED to "1",
+        ConfigurationName.PAYMENT_METHOD_MOBILE_GATEWAY to "FLUTTERWAVE",
+        ConfigurationName.PAYMENT_METHOD_MOBILE_GATEWAY_FLUTTERWAVE_SECRET_KEY to "FLT.1234567890",
+
+        ConfigurationName.SMTP_PORT to "25",
+        ConfigurationName.SMTP_HOST to "smtp.gmail.com",
+        ConfigurationName.SMTP_USERNAME to "ray.sponsible",
+        ConfigurationName.SMTP_PASSWORD to "secret",
+        ConfigurationName.SMTP_FROM_ADDRESS to "no-reply@koki.com",
+        ConfigurationName.SMTP_FROM_PERSONAL to "Koki",
+
+        ConfigurationName.STORAGE_TYPE to "S3",
+        ConfigurationName.STORAGE_S3_BUCKET to "tenant-koki",
+        ConfigurationName.STORAGE_S3_REGION to "us-west-2",
+        ConfigurationName.STORAGE_S3_SECRET_KEY to "SEC-43430409-340430-490109",
+        ConfigurationName.STORAGE_S3_ACCESS_KEY to "ACC-f000ffff-340430-490109",
+
+        ConfigurationName.TAX_AI_AGENT_ENABLED to "1",
+        ConfigurationName.TAX_EMAIL_ASSIGNEE_ENABLED to "1",
+        ConfigurationName.TAX_EMAIL_ASSIGNEE_SUBJECT to "You have a new task",
+        ConfigurationName.TAX_EMAIL_ASSIGNEE_BODY to "You have a new task assigned to you",
+        ConfigurationName.TAX_EMAIL_GATHERING_DOCUMENTS_ENABLED to "1",
+//        ConfigurationName.TAX_EMAIL_GATHERING_DOCUMENTS_SUBJECT to "The tax season has started",
+//        ConfigurationName.TAX_EMAIL_GATHERING_DOCUMENTS_BODY to "<p>Get ready for the {{taxFiscalYear}} season</p>",
+        ConfigurationName.TAX_EMAIL_DONE_ENABLED to "1",
+        ConfigurationName.TAX_EMAIL_DONE_SUBJECT to "The tax season has started",
+        ConfigurationName.TAX_EMAIL_DONE_BODY to "<p>Get ready for the {{taxFiscalYear}} season</p>",
+
+        ConfigurationName.TRANSLATION_PROVIDER to TranslationProvider.AWS.name,
+        ConfigurationName.TRANSLATION_PROVIDER_AWS_REGION to "us-east-1",
+        ConfigurationName.TRANSLATION_PROVIDER_AWS_ACCESS_KEY to "sk-540954-xoioi",
+        ConfigurationName.TRANSLATION_PROVIDER_AWS_SECRET_KEY to "sk-439093049",
+    )
+
+    // Tenants
+    val tenants = listOf(
+        Tenant(
+            id = 1,
+            name = "test",
+            domainName = "localhost",
+            locale = "CA",
+            currency = "CAD",
+            currencySymbol = "C\$",
+            timeFormat = "hh:mm a",
+            dateFormat = "yyyy-MM-dd",
+            dateTimeFormat = "yyyy-MM-dd hh:mm a",
+            numberFormat = "#,###,##0",
+            monetaryFormat = "C\$ #,###,##0.00",
+            status = TenantStatus.ACTIVE,
+            logoUrl = "https://prod-wutsi.s3.amazonaws.com/static/wutsi-blog-web/assets/wutsi/img/logo/name-104x50.png",
+            iconUrl = "https://prod-wutsi.s3.amazonaws.com/static/wutsi-blog-web/assets/wutsi/img/logo/logo_512x512.png",
+            websiteUrl = "https://www.google.com",
+            portalUrl = "http://localhost:8080",
+            clientPortalUrl = "https://localhost:8082",
+        ),
+    )
+
+    // Types
+    val types = listOf(
+        TypeSummary(id = 110, objectType = ObjectType.EMPLOYEE, name = "F", title = "Full-Time"),
+        TypeSummary(id = 111, objectType = ObjectType.EMPLOYEE, name = "P", title = "Part-Time"),
+        TypeSummary(id = 112, objectType = ObjectType.EMPLOYEE, name = "C", title = "Contractor"),
+        TypeSummary(id = 113, objectType = ObjectType.EMPLOYEE, name = "I", title = "Intern"),
+
+        TypeSummary(id = 120, objectType = ObjectType.ACCOUNT, name = "Business"),
+        TypeSummary(id = 121, objectType = ObjectType.ACCOUNT, name = "Household"),
+    )
+
+    val type = Type(
+        id = 110,
+        objectType = ObjectType.EMPLOYEE,
+        name = "F",
+        title = "Full-Time",
+        description = "Full Time Employee"
+    )
+}
