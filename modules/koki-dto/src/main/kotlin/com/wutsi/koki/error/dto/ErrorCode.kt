24130a29b01db2347041c263d33ecefc32a1ac7c
wutsi <herve.tchepannou@wutsi.com>
2025-05-30 08:24:29 -0400

    add new error codes

--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/error/dto/ErrorCode.kt
+++ modules/koki-dto/src/main/kotlin/com/wutsi/koki/error/dto/ErrorCode.kt
@@ -93,6 +93,8 @@ object ErrorCode {
     val ROOM_NOT_FOUND: String = "$PREFIX:room:not-found"
     val ROOM_INVALID_STATUS: String = "$PREFIX:room:invalid-status"
     val ROOM_IMAGE_MISSING: String = "$PREFIX:room:image-missing"
+    val ROOM_IMAGE_UNDER_REVIEW: String = "$PREFIX:room:image-under-review"
+    val ROOM_IMAGE_THRESHOLD: String = "$PREFIX:room:image-threshold"
     val ROOM_GEOLOCATION_MISSING: String = "$PREFIX:room:geolocation-missing"
     val ROOM_PRICE_MISSING: String = "$PREFIX:room:price-missing"
     val ROOM_UNIT_MISSING: String = "$PREFIX:room:unit-missing"
