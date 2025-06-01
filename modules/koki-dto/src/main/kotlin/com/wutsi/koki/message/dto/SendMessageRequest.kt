0f598e0d4cf6e9c62eadc4db4faf2538992be3e7
wutsi <herve.tchepannou@wutsi.com>
2025-06-01 08:19:43 -0400

    update schema

--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/message/dto/SendMessageRequest.kt
+++ modules/koki-dto/src/main/kotlin/com/wutsi/koki/message/dto/SendMessageRequest.kt
@@ -8,6 +8,8 @@ data class SendMessageRequest(
     @get:NotEmpty @get:Size(max = 100) val senderName: String = "",
     @get:NotEmpty @get:Size(max = 255) val senderEmail: String = "",
     @get:Size(max = 30) val senderPhone: String? = null,
+    @get:Size(max = 2) val country: String? = null,
+    @get:Size(max = 2) val language: String? = null,
     @get:NotEmpty val body: String = "",
     val owner: ObjectReference? = null
 )
