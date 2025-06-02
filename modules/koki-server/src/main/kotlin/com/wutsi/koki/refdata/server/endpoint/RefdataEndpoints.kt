319be0efdc1c8c32710382de6903d4be89611bb9
wutsi <herve.tchepannou@wutsi.com>
2025-06-02 15:44:48 -0400

    import refdata async

--- modules/koki-server/src/main/kotlin/com/wutsi/koki/refdata/server/endpoint/RefdataEndpoints.kt
+++ modules/koki-server/src/main/kotlin/com/wutsi/koki/refdata/server/endpoint/RefdataEndpoints.kt
@@ -1,6 +1,7 @@
 package com.wutsi.koki.refdata.server.endpoint
 
 import com.wutsi.koki.refdata.server.service.RefdataService
+import org.springframework.scheduling.annotation.Async
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 class RefdataEndpoints(
     val service: RefdataService,
 ) {
+    @Async
     @GetMapping("/import")
     fun import() {
         service.import()
