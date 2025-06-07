d93cfb81710c7e86dcb85e17c4e54ca740c18841
wutsi <herve.tchepannou@wutsi.com>
2025-06-07 18:08:29 -0400

    copy webasset to s3

--- modules/koki-room-web/src/main/kotlin/com/wutsi/koki/room/web/refdata/mapper/AmenityMapper.kt
+++ modules/koki-room-web/src/main/kotlin/com/wutsi/koki/room/web/refdata/mapper/AmenityMapper.kt
@@ -9,13 +9,15 @@ import org.springframework.stereotype.Service
 class AmenityMapper : TenantAwareMapper() {
     companion object {
         val TOP_AMENITIES_ICONS = mapOf(
-            1001 to "fa-solid fa-droplet", // Runnin Water
+            1000 to "fa-regular fa-lightbulb", // Electricity
+            1001 to "fa-solid fa-droplet", // Running Water
             1003L to "fa-solid fa-snowflake", // Air Conditioning
             1005L to "fa-solid fa-wifi", // Wifi
             1030L to "fa-solid fa-lock", // Locker
             1031L to "fa-solid fa-tv", // TV
             1039L to "fa-solid fa-gamepad", // Video Game Console
             1046L to "fa-solid fa-person-swimming", // Swimming pool
+            1052L to "fa-solid fa-umbrella-beach", // Beach access
             1055L to "fa-solid fa-square-parking", // Free Parking
             1059L to "fa-solid fa-square-parking", // Shuttle Service
             1065L to "fa-solid fa-lock", // Smart Lock
