c980aa3f61114719feaf2a64af56512f346a30b6
wutsi <herve.tchepannou@wutsi.com>
2025-06-19 17:26:53 -0400

    add more test

--- modules/koki-portal/src/main/kotlin/com/wutsi/koki/portal/account/page/AccountSelectorController.kt
+++ modules/koki-portal/src/main/kotlin/com/wutsi/koki/portal/account/page/AccountSelectorController.kt
@@ -8,16 +8,20 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequiresPermission(["account"])
+@RequiresPermission(["account", "account:full_access"])
 class AccountSelectorController(private val service: AccountService) : AbstractAccountController() {
     @GetMapping("/accounts/selector/search")
     fun search(
         @RequestParam(required = false, name = "q") keyword: String? = null,
+        @RequestParam(required = false) limit: Int = 20,
+        @RequestParam(required = false) offset: Int = 0,
     ): List<AccountModel> {
         val user = userHolder.get()
         return service.accounts(
             keyword = keyword,
             managedByIds = if (user?.hasFullAccess("account") == true) emptyList() else listOf(user?.id ?: -1),
+            limit = limit,
+            offset = offset,
             fullGraph = false,
         )
     }
