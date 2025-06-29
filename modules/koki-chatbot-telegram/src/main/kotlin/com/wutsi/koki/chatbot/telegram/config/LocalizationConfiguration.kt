7458a264e313f637ff474577af849bab00cbb7da
wutsi <herve.tchepannou@wutsi.com>
2025-06-29 19:27:51 -0400

    update translation configuration

--- modules/koki-chatbot-telegram/src/main/kotlin/com/wutsi/koki/chatbot/telegram/config/LocalizationConfiguration.kt
+++ modules/koki-chatbot-telegram/src/main/kotlin/com/wutsi/koki/chatbot/telegram/config/LocalizationConfiguration.kt
@@ -1,9 +1,12 @@
 package com.wutsi.koki.chatbot.telegram.config
 
+import org.springframework.context.MessageSource
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.support.ReloadableResourceBundleMessageSource
 import org.springframework.web.servlet.LocaleResolver
 import org.springframework.web.servlet.i18n.SessionLocaleResolver
+import java.nio.charset.StandardCharsets
 import java.util.Locale
 
 @Configuration
@@ -13,5 +16,15 @@ class LocalizationConfiguration {
         val slr = SessionLocaleResolver()
         slr.setDefaultLocale(Locale.FRENCH)
         return slr
+    }
+
+    @Bean
+    fun messageSource(): MessageSource {
+        val messageSource = ReloadableResourceBundleMessageSource()
+        messageSource.setBasenames("classpath:/messages")
+        messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name())
+        messageSource.setFallbackToSystemLocale(false)
+        messageSource.setUseCodeAsDefaultMessage(true)
+        return messageSource
     }
 }
