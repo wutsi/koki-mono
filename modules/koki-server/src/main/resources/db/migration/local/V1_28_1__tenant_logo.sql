c60281c15808d3a12ced008a63f1f2c8b44b659c
wutsi <herve.tchepannou@wutsi.com>
2025-07-07 20:01:12 -0400

    update logo

---
+++ modules/koki-server/src/main/resources/db/migration/local/V1_28_1__tenant_logo.sql
@@ -0,0 +1,5 @@
+UPDATE T_TENANT set
+    name='koki',
+    icon_url='https://com-wutsi-koki-test.s3.us-east-1.amazonaws.com/tenant/1/logo/icon.png',
+    logo_url='https://com-wutsi-koki-test.s3.us-east-1.amazonaws.com/tenant/1/logo/logo.png';
+
