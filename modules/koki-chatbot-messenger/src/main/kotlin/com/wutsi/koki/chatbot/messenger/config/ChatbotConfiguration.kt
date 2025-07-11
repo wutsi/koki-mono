ef7154396e81c532398f1f39f9b1b43db110584d
wutsi <herve.tchepannou@wutsi.com>
2025-07-11 17:28:36 -0400

    add ai configuration

--- modules/koki-chatbot-messenger/src/main/kotlin/com/wutsi/koki/chatbot/messenger/config/ChatbotConfiguration.kt
+++ modules/koki-chatbot-messenger/src/main/kotlin/com/wutsi/koki/chatbot/messenger/config/ChatbotConfiguration.kt
@@ -3,7 +3,7 @@ package com.wutsi.koki.chatbot.messenger.config
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.wutsi.koki.chatbot.Chatbot
 import com.wutsi.koki.chatbot.ai.agent.AgentFactory
-import com.wutsi.koki.platform.ai.llm.LLM
+import com.wutsi.koki.platform.ai.llm.LLMBuilder
 import com.wutsi.koki.sdk.KokiRefData
 import com.wutsi.koki.sdk.KokiRooms
 import org.springframework.context.annotation.Bean
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 class ChatbotConfiguration(
-    private val llm: LLM,
+    private val llmBuilder: LLMBuilder,
     private val kokiRooms: KokiRooms,
     private val kokiRefData: KokiRefData,
     private val objectMapper: ObjectMapper,
@@ -19,7 +19,7 @@ class ChatbotConfiguration(
     @Bean
     fun agentFactory(): AgentFactory {
         return AgentFactory(
-            llm = llm,
+            llm = llmBuilder.default(),
         )
     }
 
