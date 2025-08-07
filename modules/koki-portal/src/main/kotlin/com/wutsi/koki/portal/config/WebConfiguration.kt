ec2d37c8ef1b4f839f1bd77d2a17884792127007
wutsi <herve.tchepannou@wutsi.com>
2025-08-07 13:13:18 -0400

    add mockup for creating agent account

--- modules/koki-portal/src/main/kotlin/com/wutsi/koki/portal/config/WebConfiguration.kt
+++ modules/koki-portal/src/main/kotlin/com/wutsi/koki/portal/config/WebConfiguration.kt
@@ -2,13 +2,46 @@ package com.wutsi.koki.portal.config
 
 import com.wutsi.koki.portal.security.RequirePermissionInterceptor
 import com.wutsi.koki.portal.user.service.CurrentUserHolder
+import org.springframework.context.MessageSource
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.support.ReloadableResourceBundleMessageSource
+import org.springframework.web.servlet.LocaleResolver
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor
+import org.springframework.web.servlet.i18n.SessionLocaleResolver
+import java.nio.charset.StandardCharsets
+import java.util.Locale
 
 @Configuration
 class WebConfiguration(private val currentUser: CurrentUserHolder) : WebMvcConfigurer {
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(RequirePermissionInterceptor(currentUser))
+        registry.addInterceptor(localeChangeInterceptor())
+    }
+
+    @Bean
+    fun localeChangeInterceptor(): LocaleChangeInterceptor {
+        val lci = LocaleChangeInterceptor()
+        lci.setParamName("lang")
+        return lci
+    }
+
+    @Bean
+    fun localeResolver(): LocaleResolver {
+        val slr = SessionLocaleResolver()
+        slr.setDefaultLocale(Locale.FRENCH)
+        return slr
+    }
+
+    @Bean
+    fun messageSource(): MessageSource {
+        val messageSource = ReloadableResourceBundleMessageSource()
+        messageSource.setBasenames("classpath:/messages")
+        messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name())
+        messageSource.setFallbackToSystemLocale(false)
+        messageSource.setUseCodeAsDefaultMessage(true)
+        return messageSource
     }
 }
