12f5646f3b28f8e8e9bf3a165e8e09ab4d469520
wutsi <herve.tchepannou@wutsi.com>
2025-08-11 19:33:10 -0400

    update dependencies

--- modules/koki-portal/src/main/kotlin/com/wutsi/koki/portal/listing/page/PublishDoneListingController.kt
+++ modules/koki-portal/src/main/kotlin/com/wutsi/koki/portal/listing/page/PublishDoneListingController.kt
@@ -1,13 +1,10 @@
 package com.wutsi.koki.portal.listing.page
 
 import com.wutsi.koki.portal.common.page.PageName
-import com.wutsi.koki.portal.listing.form.ListingForm
 import com.wutsi.koki.portal.security.RequiresPermission
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ModelAttribute
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 
