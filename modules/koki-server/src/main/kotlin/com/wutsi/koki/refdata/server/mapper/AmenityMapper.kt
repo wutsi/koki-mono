e823517c3b0e4f27782f7c58a2c27400fe2375ee
wutsi <herve.tchepannou@wutsi.com>
2025-08-11 19:29:02 -0400

    update schema

--- modules/koki-server/src/main/kotlin/com/wutsi/koki/refdata/server/mapper/AmenityMapper.kt
+++ modules/koki-server/src/main/kotlin/com/wutsi/koki/refdata/server/mapper/AmenityMapper.kt
@@ -2,21 +2,17 @@ package com.wutsi.koki.refdata.server.mapper
 
 import com.wutsi.koki.refdata.dto.Amenity
 import com.wutsi.koki.refdata.server.domain.AmenityEntity
-import org.springframework.context.i18n.LocaleContextHolder
 import org.springframework.stereotype.Service
 
 @Service
 class AmenityMapper {
     fun toAmenity(entity: AmenityEntity): Amenity {
-        val language = LocaleContextHolder.getLocale().language
         return Amenity(
             id = entity.id,
             active = entity.active,
             categoryId = entity.categoryId,
-            name = when (language) {
-                "fr" -> entity.nameFr ?: entity.name
-                else -> entity.name
-            },
+            name = entity.name,
+            nameFr = entity.nameFr,
             icon = entity.icon,
         )
     }
