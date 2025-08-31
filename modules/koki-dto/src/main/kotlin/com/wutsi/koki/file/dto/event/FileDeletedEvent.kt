07890b0347ec4714784d9b1c5f67af5d00982206
wutsi <herve.tchepannou@wutsi.com>
2025-08-30 23:03:53 -0400

    update delete event

--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/file/dto/event/FileDeletedEvent.kt
+++ modules/koki-dto/src/main/kotlin/com/wutsi/koki/file/dto/event/FileDeletedEvent.kt
@@ -1,7 +1,10 @@
 package com.wutsi.koki.file.dto.event
 
+import com.wutsi.koki.common.dto.ObjectReference
+
 data class FileDeletedEvent(
     val fileId: Long = -1,
     val tenantId: Long = -1,
+    val owner: ObjectReference? = null,
     val timestamp: Long = System.currentTimeMillis(),
 )
