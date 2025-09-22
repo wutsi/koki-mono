4b4827e497796a28a1a48c05770e883c1a0a3f0a
wutsi <herve.tchepannou@wutsi.com>
2025-09-22 08:21:51 -0400

    add file-type

--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/file/dto/event/FileUploadedEvent.kt
+++ modules/koki-dto/src/main/kotlin/com/wutsi/koki/file/dto/event/FileUploadedEvent.kt
@@ -1,10 +1,12 @@
 package com.wutsi.koki.file.dto.event
 
 import com.wutsi.koki.common.dto.ObjectReference
+import com.wutsi.koki.file.dto.FileType
 
 data class FileUploadedEvent(
     val fileId: Long = -1,
     val tenantId: Long = -1,
+    val fileType: FileType = FileType.UNKNOWN,
     val owner: ObjectReference? = null,
     val timestamp: Long = System.currentTimeMillis(),
 )
