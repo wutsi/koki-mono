1fc4cdc9528f29a1137348bf873207ebf45f3b45
wutsi <herve.tchepannou@wutsi.com>
2025-11-06 16:25:28 -0500

    update dto

--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/listing/dto/CloseListingRequest.kt
+++ modules/koki-dto/src/main/kotlin/com/wutsi/koki/listing/dto/CloseListingRequest.kt
@@ -1,6 +1,13 @@
 package com.wutsi.koki.listing.dto
 
+import java.util.Date
+
 data class CloseListingRequest(
     val status: ListingStatus = ListingStatus.UNKNOWN,
     val comment: String? = null,
+    val salePrice: Long? = null,
+    val soldAt: Date? = null,
+    val closedOfferId: Long? = null,
+    val buyerContactId: Long? = null,
+    val buyerAgentUserId: Long? = null,
 )
