7d73eda2e76a56b2f56bc40a63ad6cd344c22908
wutsi <herve.tchepannou@wutsi.com>
2025-11-16 02:33:17 -0500

    fix tklint issue

--- modules/koki-portal/src/main/kotlin/com/wutsi/koki/portal/lead/page/AbstractLeadController.kt
+++ modules/koki-portal/src/main/kotlin/com/wutsi/koki/portal/lead/page/AbstractLeadController.kt
@@ -1,10 +1,6 @@
 package com.wutsi.koki.portal.lead.page
 
-import com.wutsi.koki.portal.listing.model.ListingModel
-import com.wutsi.koki.portal.listing.service.ListingService
 import com.wutsi.koki.portal.module.page.AbstractModulePageController
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.servlet.mvc.AbstractController
 
 abstract class AbstractLeadController : AbstractModulePageController() {
     companion object {
--- modules/koki-portal/src/main/kotlin/com/wutsi/koki/portal/lead/page/LeadController.kt
+++ modules/koki-portal/src/main/kotlin/com/wutsi/koki/portal/lead/page/LeadController.kt
@@ -1,25 +1,12 @@
 package com.wutsi.koki.portal.lead.page
 
-import com.wutsi.koki.lead.dto.LeadStatus
-import com.wutsi.koki.listing.dto.ListingStatus
-import com.wutsi.koki.listing.dto.ListingType
-import com.wutsi.koki.listing.dto.PropertyType
-import com.wutsi.koki.portal.common.model.MoneyModel
 import com.wutsi.koki.portal.common.page.PageName
-import com.wutsi.koki.portal.lead.model.LeadModel
-import com.wutsi.koki.portal.listing.model.ListingModel
-import com.wutsi.koki.portal.refdata.model.AddressModel
-import com.wutsi.koki.portal.refdata.model.LocationModel
 import com.wutsi.koki.portal.security.RequiresPermission
-import io.micrometer.core.instrument.Metrics.more
-import org.apache.commons.lang3.time.DateUtils
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import java.util.Date
 
 @Controller
 @RequestMapping("/leads")
