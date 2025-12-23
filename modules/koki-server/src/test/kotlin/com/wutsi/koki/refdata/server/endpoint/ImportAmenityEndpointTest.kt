b763fcd821d0b32235b3c3d14cf690ca9dbba015
wutsi <herve.tchepannou@wutsi.com>
2025-12-23 11:41:41 -0500

    fix test

--- modules/koki-server/src/test/kotlin/com/wutsi/koki/refdata/server/endpoint/ImportAmenityEndpointTest.kt
+++ modules/koki-server/src/test/kotlin/com/wutsi/koki/refdata/server/endpoint/ImportAmenityEndpointTest.kt
@@ -22,7 +22,7 @@ class ImportAmenityEndpointTest : AuthorizationAwareEndpointTest() {
 
         assertEquals(HttpStatus.OK, response.statusCode)
 
-        assertEquals(65, dao.findAll().toList().size)
+        assertEquals(68, dao.findAll().toList().size)
 
         var amenity = dao.findById(1000).get()
         assertEquals(40000, amenity.categoryId)
