443b149fcaf41ea330bccf314c3f97b51b197b67
wutsi <herve.tchepannou@wutsi.com>
2025-12-24 10:30:14 -0500

    simplify metric display

--- modules/koki-portal-public/src/main/kotlin/com/wutsi/koki/portal/pub/agent/model/AgentModel.kt
+++ modules/koki-portal-public/src/main/kotlin/com/wutsi/koki/portal/pub/agent/model/AgentModel.kt
@@ -1,5 +1,7 @@
 package com.wutsi.koki.portal.pub.agent.model
 
+import com.wutsi.koki.platform.util.NumberUtils
+import com.wutsi.koki.portal.pub.common.model.MoneyModel
 import com.wutsi.koki.portal.pub.user.model.UserModel
 
 data class AgentModel(
@@ -14,4 +16,15 @@ data class AgentModel(
     val totalRentalsMetric: AgentMetricModel? = null,
     val past12mRentalsMetric: AgentMetricModel? = null,
     val publicUrl: String = "",
-)
+) {
+    fun priceRangeText(min: MoneyModel, max: MoneyModel): String {
+        if (min.amount == max.amount) {
+            return min.shortText
+        } else {
+            val currency = min.shortText.split(" ").firstOrNull() ?: ""
+            return currency + " " +
+                NumberUtils.shortText(min.amount.toLong()) + " - " +
+                NumberUtils.shortText(max.amount.toLong())
+        }
+    }
+}
--- modules/koki-portal/src/main/kotlin/com/wutsi/koki/portal/agent/model/AgentModel.kt
+++ modules/koki-portal/src/main/kotlin/com/wutsi/koki/portal/agent/model/AgentModel.kt
@@ -1,5 +1,7 @@
 package com.wutsi.koki.portal.agent.model
 
+import com.wutsi.koki.platform.util.NumberUtils
+import com.wutsi.koki.portal.common.model.MoneyModel
 import com.wutsi.koki.portal.user.model.UserModel
 
 data class AgentModel(
@@ -20,4 +22,15 @@ data class AgentModel(
     val past12mSalesMetric: AgentMetricModel? = null,
     val totalRentalsMetric: AgentMetricModel? = null,
     val past12mRentalsMetric: AgentMetricModel? = null,
-)
+) {
+    fun priceRangeText(min: MoneyModel, max: MoneyModel): String {
+        if (min.amount == max.amount) {
+            return min.shortText
+        } else {
+            val currency = min.shortText.split(" ").firstOrNull() ?: ""
+            return currency + " " +
+                NumberUtils.shortText(min.amount.toLong()) + " - " +
+                NumberUtils.shortText(max.amount.toLong())
+        }
+    }
+}
