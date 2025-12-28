431f8e3671f782a39a49ff3c0551c7c2f1e5a502
wutsi <herve.tchepannou@wutsi.com>
2025-12-27 21:38:54 -0500

    update place dto

--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/place/dto/CreatePlaceRequest.kt
+++ modules/koki-dto/src/main/kotlin/com/wutsi/koki/place/dto/CreatePlaceRequest.kt
@@ -1,7 +1,10 @@
 package com.wutsi.koki.place.dto
 
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.Size
+
 data class CreatePlaceRequest(
-    val name: String = "",
+    @get:NotEmpty @get:Size(max = 100) val name: String = "",
     val type: PlaceType = PlaceType.UNKNOWN,
     val neighbourhoodId: Long = -1,
 )
--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/place/dto/PlaceSummary.kt
+++ modules/koki-dto/src/main/kotlin/com/wutsi/koki/place/dto/PlaceSummary.kt
@@ -3,10 +3,9 @@ package com.wutsi.koki.place.dto
 data class PlaceSummary(
     val id: Long = -1,
     val heroImageId: Long? = null,
-    val neighbourhoodId: Long? = null,
+    val neighbourhoodId: Long = -1,
     val type: PlaceType = PlaceType.UNKNOWN,
     val name: String = "",
-    val nameFr: String? = null,
     val summary: String? = null,
     val summaryFr: String? = null,
     val rating: Double? = null,
