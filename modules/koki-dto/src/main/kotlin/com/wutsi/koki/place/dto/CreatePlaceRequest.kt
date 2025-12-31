4c2dded7a5cce4e54199e5f3b59c637ead36e77c
wutsi <herve.tchepannou@wutsi.com>
2025-12-31 12:23:28 -0500

    add flag to generate content

--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/place/dto/CreatePlaceRequest.kt
+++ modules/koki-dto/src/main/kotlin/com/wutsi/koki/place/dto/CreatePlaceRequest.kt
@@ -7,4 +7,5 @@ data class CreatePlaceRequest(
     @get:NotEmpty @get:Size(max = 100) val name: String = "",
     val type: PlaceType = PlaceType.UNKNOWN,
     val neighbourhoodId: Long = -1,
+    val generateContent: Boolean = true,
 )
