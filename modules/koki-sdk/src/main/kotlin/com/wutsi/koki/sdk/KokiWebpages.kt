a79ff8e202c0fc1f6c591021829eb47299591e7d
wutsi <herve.tchepannou@wutsi.com>
2026-01-21 17:07:56 -0500

    update sdk

---
+++ modules/koki-sdk/src/main/kotlin/com/wutsi/koki/sdk/KokiWebpages.kt
@@ -0,0 +1,39 @@
+package com.wutsi.koki.sdk
+
+import com.wutsi.koki.webscraping.dto.GetWebpageResponse
+import com.wutsi.koki.webscraping.dto.SearchWebpageResponse
+import org.springframework.web.client.RestTemplate
+
+class KokiWebpages(
+    private val urlBuilder: URLBuilder,
+    private val rest: RestTemplate,
+) {
+    companion object {
+        private const val PATH_PREFIX = "/v1/webpages"
+    }
+
+    fun get(id: Long): GetWebpageResponse {
+        val url = urlBuilder.build("$PATH_PREFIX/$id")
+        return rest.getForEntity(url, GetWebpageResponse::class.java).body!!
+    }
+
+    fun search(
+        websiteId: Long? = null,
+        listingId: Long? = null,
+        active: Boolean? = null,
+        limit: Int = 20,
+        offset: Int = 0,
+    ): SearchWebpageResponse {
+        val url = urlBuilder.build(
+            PATH_PREFIX,
+            mapOf(
+                "website-id" to websiteId,
+                "listing-id" to listingId,
+                "active" to active,
+                "limit" to limit,
+                "offset" to offset,
+            ),
+        )
+        return rest.getForEntity(url, SearchWebpageResponse::class.java).body!!
+    }
+}
---
+++ modules/koki-sdk/src/main/kotlin/com/wutsi/koki/sdk/KokiWebsites.kt
@@ -0,0 +1,59 @@
+package com.wutsi.koki.sdk
+
+import com.wutsi.koki.webscraping.dto.CreateWebsiteRequest
+import com.wutsi.koki.webscraping.dto.CreateWebsiteResponse
+import com.wutsi.koki.webscraping.dto.GetWebsiteResponse
+import com.wutsi.koki.webscraping.dto.ScrapeWebsiteRequest
+import com.wutsi.koki.webscraping.dto.ScrapeWebsiteResponse
+import com.wutsi.koki.webscraping.dto.SearchWebsiteResponse
+import com.wutsi.koki.webscraping.dto.UpdateWebsiteRequest
+import org.springframework.web.client.RestTemplate
+
+class KokiWebsites(
+    private val urlBuilder: URLBuilder,
+    private val rest: RestTemplate,
+) {
+    companion object {
+        private const val PATH_PREFIX = "/v1/websites"
+    }
+
+    fun create(request: CreateWebsiteRequest): CreateWebsiteResponse {
+        val url = urlBuilder.build(PATH_PREFIX)
+        return rest.postForEntity(url, request, CreateWebsiteResponse::class.java).body!!
+    }
+
+    fun update(id: Long, request: UpdateWebsiteRequest) {
+        val url = urlBuilder.build("$PATH_PREFIX/$id")
+        rest.postForEntity(url, request, Any::class.java)
+    }
+
+    fun get(id: Long): GetWebsiteResponse {
+        val url = urlBuilder.build("$PATH_PREFIX/$id")
+        return rest.getForEntity(url, GetWebsiteResponse::class.java).body!!
+    }
+
+    fun search(
+        ids: List<Long> = emptyList(),
+        userIds: List<Long> = emptyList(),
+        active: Boolean? = null,
+        limit: Int = 20,
+        offset: Int = 0,
+    ): SearchWebsiteResponse {
+        val url = urlBuilder.build(
+            PATH_PREFIX,
+            mapOf(
+                "id" to ids,
+                "user-id" to userIds,
+                "active" to active,
+                "limit" to limit,
+                "offset" to offset,
+            ),
+        )
+        return rest.getForEntity(url, SearchWebsiteResponse::class.java).body!!
+    }
+
+    fun scrape(id: Long, request: ScrapeWebsiteRequest): ScrapeWebsiteResponse {
+        val url = urlBuilder.build("$PATH_PREFIX/$id/scrape")
+        return rest.postForEntity(url, request, ScrapeWebsiteResponse::class.java).body!!
+    }
+}
