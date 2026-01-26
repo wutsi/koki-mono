f112662480e842abd0c3c172961920517c4e1c7c
wutsi <herve.tchepannou@wutsi.com>
2026-01-26 07:26:38 -0500

    add recipient id

--- modules/koki-dto/src/main/kotlin/com/wutsi/koki/track/dto/Track.kt
+++ modules/koki-dto/src/main/kotlin/com/wutsi/koki/track/dto/Track.kt
@@ -22,4 +22,5 @@ data class Track(
     val channelType: ChannelType = ChannelType.UNKNOWN,
     val rank: Int? = null,
     val productType: ObjectType? = null,
+    val recipientId: Long? = null,
 )
