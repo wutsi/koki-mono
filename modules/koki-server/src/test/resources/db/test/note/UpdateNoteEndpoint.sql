b05b67c3d29eec3ca6fc4ae2b4118b5956cb35b4
wutsi <herve.tchepannou@wutsi.com>
2026-01-26 12:42:47 -0500

    remove module

--- modules/koki-server/src/test/resources/db/test/note/UpdateNoteEndpoint.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO T_NOTE(id, tenant_fk, subject, body, deleted)
-    VALUES (100, 1, 'Yo', 'Man', false),
-           (199, 1, 'Yo', 'Man', true),
-           (200, 2, 'Yo', 'Man', false);
