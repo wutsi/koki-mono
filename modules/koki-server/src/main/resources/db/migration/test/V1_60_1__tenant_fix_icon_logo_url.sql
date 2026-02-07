bb7cbb0d10a01998565bfd779844d5ce50e31a73
wutsi <herve.tchepannou@wutsi.com>
2026-02-07 17:21:49 -0500

    optimize tenant and user images

---
+++ modules/koki-server/src/main/resources/db/migration/test/V1_60_1__tenant_fix_icon_logo_url.sql
@@ -0,0 +1,3 @@
+UPDATE T_TENANT SET logo_url='https://s3.amazonaws.com/com-wutsi-koki-test/tenant/1/logo/logo.png',
+                    icon_url='https://s3.amazonaws.com/com-wutsi-koki-test/tenant/1/logo/icon.png'
+WHERE id=1;
